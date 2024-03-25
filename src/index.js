312e855691d4b1051a4ea4febc5d650fd822ea24
VexScythe <davide.ludovici@yahoo.it>
2024-03-25 18:04:31 +0100

    feat: finish style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import vcheck from './vcheck.png';
+import addImg from './add.svg';
+import modifyImg from './modify.svg';
+import deleteImg from './delete.svg';
 
 
-console.log("ciao")
+
