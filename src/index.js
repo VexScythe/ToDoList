d82f518402a39b00a8a8d33f251adc9ec9bb171f
VexScythe <davide.ludovici@yahoo.it>
2024-03-27 16:28:27 +0100

    feat: change list/task name form

--- src/index.js
+++ src/index.js
@@ -11,11 +11,20 @@ const taskTitleElement = document.querySelector('[data-task-title]');
 const taskContainer = document.querySelector('[data-tasks]')
 const newTaskForm = document.querySelector('[data-new-task-form');
 const newTaskInput = document.querySelector('[data-new-task-input]');
+const newNameContainer = document.querySelector('[data-new-name-container]')
+const newNameTitle = document.querySelector('[data-new-name-title]');
+const newNameForm = document.querySelector('[data-new-name-form]');
+const newNameInput = document.querySelector('[data-new-name-input]');
+const newTaskNameContainer = document.querySelector('[data-new-name-task-container]')
+const newTaskNameTitle = document.querySelector('[data-new-name-task-title]');
+const newTaskNameForm = document.querySelector('[data-new-name-task-form]');
+const newTaskNameInput = document.querySelector('[data-new-name-task-input]');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+let selectedTaskId = null;
 
 listsContainer.addEventListener('click', e => {
     if (e.target.classList.contains('list__name')){
@@ -52,6 +61,33 @@ newTaskForm.addEventListener("submit", e => {
     const selectedList = lists.find(list => list.id === selectedListId);
     selectedList.tasks.push(task);
     saveAndRender();
+});
+
+newNameForm.addEventListener("submit", e => {
+    e.preventDefault();
+    if (newNameInput.value == null || newNameInput.value === ''){
+        newNameContainer.style.display = "none";
+    }else{
+        const selectedList = lists.find(list => list.id === selectedListId);
+        selectedList.name = newNameInput.value;
+        newNameInput.value = null;
+        newNameContainer.style.display = "none";
+        saveAndRender();
+    }
+});
+
+newTaskNameForm.addEventListener("submit", e => {
+    e.preventDefault();
+    if (newTaskNameInput.value == null || newTaskNameInput.value === ''){
+        newTaskNameContainer.style.display = "none";
+    }else{
+        const selectedList = lists.find(list => list.id === selectedListId);
+        const selectedTask = selectedList.tasks.find(task => task.id === selectedTaskId);
+        selectedTask.name = newTaskNameInput.value;
+        newTaskNameInput.value = null;
+        newTaskNameContainer.style.display = "none";
+        saveAndRender();
+    }
 });
 
 function createList(name) {
@@ -103,7 +139,15 @@ function renderLists() {
         listName.innerText = list.name;
         listDiv.appendChild(listName);
         const btnModify = document.createElement('button');
+        btnModify.dataset.listId = list.id;
         btnModify.classList.add('btn');
+        btnModify.addEventListener('click', () => {
+            selectedListId = list.id;
+            newNameTitle.innerText = `List:${list.name} - New Name:`
+            newNameContainer.style.display = 'flex';
+            newNameInput.focus();
+            saveAndRender();
+        })
         listDiv.appendChild(btnModify);
         const btnDelete = document.createElement('button');
         btnDelete.classList.add('btn');
@@ -147,7 +191,14 @@ function renderTasks(selectedList) {
         label.innerText = task.name;
         taskDiv.appendChild(label);
         const btnModify = document.createElement('button');
-        btnModify.classList.add('btn');
+        btnModify.addEventListener('click', () => {
+            selectedTaskId = task.id;
+            newTaskNameTitle.innerText = `Task:${task.name} - New Name:`
+            newTaskNameContainer.style.display = 'flex';
+            newTaskNameInput.focus();
+            saveAndRender();
+        })
+        btnModify.classList.add('btn', 'btn__modify');
         taskDiv.appendChild(btnModify);
         const btnDelete = document.createElement('button');
         btnDelete.classList.add('btn');
